cbc621d3c00ace3ff82da3dee53bd57d1b0d6378
xzh <xzh@applent.com>
2025-12-05 16:49:43 -0500

    Refactor dataset download script to remove unnecessary imports and simplify path handling

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,15 +1,9 @@
 # Install dependencies as needed:
 # pip install kagglehub
-import config
-
-# Set the path to the file you'd like to load
-file_path = config.DATASET_DIR
 
 import kagglehub
-import os
 
 # Download latest version to specified directory
-os.makedirs(file_path, exist_ok=True)
-path = kagglehub.dataset_download("picekl/geoplant", path=file_path)
+path = kagglehub.dataset_download("picekl/geoplant")
 
 print("Path to dataset files:", path)
