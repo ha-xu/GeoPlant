c0613914945f03b1e57c8b8cd519902332b99d78
xzh <xzh@applent.com>
2025-12-04 23:32:13 -0500

    Add functionality to list files in the dataset directory

--- src/run.py
+++ src/run.py
@@ -1,8 +1,12 @@
 import config
+import os
 
 def print_paths():
     print("Dataset Directory:", config.DATASET_DIR)
     print("Model Directory:", config.MODEL_DIR)
+    # print the files in dataset directory (optional)
+    print("Files in Dataset Directory:", os.listdir(config.DATASET_DIR))
+
 
 if __name__ == "__main__":
     print_paths()
