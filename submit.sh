ce86af47b78a1be9d7f2de3165b97e5cf3a771dc
xzh <xzh@applent.com>
2025-12-04 23:41:01 -0500

    Add SLURM job submission script and refactor run script

--- submit.sh
+++ submit.sh
@@ -9,20 +9,26 @@
 #SBATCH --error=/scratch/zx1875/EoDL/logs/%x-%j.err
 #SBATCH --chdir=/home/zx1875/EoDL/search-and-learn 
 
+# THIS SCRIPT FILE SHOULD NEVER CHANGE
 
 export MAIN_FOLDER=/home/zx1875/EoDL
-export DATA_FOLDER=/scratch/zx1875/EoDL/dataset
-export LOG_FOLDER=/scratch/zx1875/EoDL/logs
+# export DATA_FOLDER=/scratch/zx1875/EoDL/dataset
+# export LOG_FOLDER=/scratch/zx1875/EoDL/logs
 
 # create log folder if it doesn't exist
 mkdir -p $LOG_FOLDER
 
-# 2. Print job information
+# Print job information
 echo "Job starting on $(hostname)"
 echo "Job ID: $SLURM_JOB_ID"
 
 nvidia-smi
 
-python src/run.py
-
+# Navigate to the main project directory
+cd $MAIN_FOLDER
+# Pull the latest code from the repository
+git pull
+# Run the script
+bash scripts/run.sh
+    
 echo "job finished."
